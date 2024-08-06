e3e1452b2a73ad6eebfdb0d318bb045145d77c40
thangpd <phamducthang18cm@gmail.com>
2024-08-06 21:15:49 +0700

    add code in company

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -14,6 +14,10 @@ class TaskResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'is_completed' => (bool) $this->is_completed,
+        ];
     }
 }
